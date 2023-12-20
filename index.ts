c38c1dba7c6527fd66b6a1fd53702171ce9031e9
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-12-20 15:46:44 +0700

    lấy ra danh sách tour

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import sequelize from "./config/database";
+import Tour from "./models/tour.model";
 sequelize;
 
 const app: Express = express();
@@ -11,8 +12,14 @@ const port: number | string = process.env.PORT || 3012;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/tours", (req: Request, res: Response) => {
-  res.render("client/pages/tours/index");
+app.get("/tours", async (req: Request, res: Response) => {
+  const tours = await Tour.findAll({
+    raw: true
+  });
+
+  res.render("client/pages/tours/index", {
+    tours: tours
+  });
 });
 
 app.listen(port, () => {
