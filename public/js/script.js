bd6598aecf2d359392243275ed7e9e70ef0381bc
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-12-22 15:57:02 +0700

    slide ảnh

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,18 @@
-console.log('ok')
+// swiper for tour detail
+var imagesThumb = new Swiper(".imagesThumb", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var imagesMain = new Swiper(".imagesMain", {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: imagesThumb,
+  },
+});
+// end swiper for tour detail
