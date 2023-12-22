ddbfbf4603ff6dcddeef1af61c48efe016571ca2
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-12-22 16:22:33 +0700

    thêm tour vào giỏ hàng dùng local storage

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,40 @@ var imagesMain = new Swiper(".imagesMain", {
   },
 });
 // end swiper for tour detail
+
+// Carts
+const cart = localStorage.getItem("cart");
+
+// if cart is empty or not exist in local storage then create cart object in local storage with empty array value
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]));
+}
+
+// add tour to cart
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if (formAddToCart) {
+  formAddToCart.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const quantity = parseInt(event.target.elements.quantity.value);
+    const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+
+    if (quantity > 0 && tourId) {
+      const cart = JSON.parse(localStorage.getItem("cart"));
+
+      const indexExistTour = cart.findIndex((item) => item.tourId === tourId);
+
+      if (indexExistTour !== -1) {
+        cart[indexExistTour].quantity += quantity;
+      } else {
+        cart.push({
+          tourId,
+          quantity,
+        });
+      }
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  });
+}
+
+// end Carts
