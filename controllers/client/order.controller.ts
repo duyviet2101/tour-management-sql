55b40afde90f77ba2bdea7448423621d55597e24
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-12-23 21:46:30 +0700

    lưu data vào bảng order

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,11 +1,30 @@
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate";
 
 // [POST] /order/
 export const order = async (req: Request, res: Response) => {
   const data = req.body;
-  console.log(data);
+  
+  const dataOrder = {
+    code: "",
+    fullName: data.info.fullName,
+    phone: data.info.phone,
+    note: data.info.note,
+    status: "initial",
+  }
+
+  console.log(dataOrder);
+
+  const order = await Order.create(dataOrder);
+
+  const orderId = order.dataValues.id;
+  const code = generateOrderCode(orderId);
+  await order.update({ code });
+
   res.json({
     code:200,
-    message:"dat hang thanh cong"
+    message:"dat hang thanh cong",
+    orderCode: code
   });
 };
